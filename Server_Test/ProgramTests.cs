f32bcd1581faed2d19892cd0f2f1d15695efb331
juzeg <juzeg@DESKTOP-9FQCCN1>
2018-01-05 20:40:07 +0100

    initial tests

--- Server_Test/ProgramTests.cs
+++ Server_Test/ProgramTests.cs
@@ -1,5 +1,6 @@
 ﻿#region
 
+using System.Diagnostics.Eventing.Reader;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 #endregion
@@ -9,5 +10,36 @@ namespace Server_Test
     [TestClass]
     public class ProgramTests
     {
+
+        [TestMethod]
+        public void AdditionTest_4p5_e9()
+        {
+            Assert.Equals(9, Addition(4, 5));
+
+        }
+        [TestMethod]
+        public void EnumInterpretatorTest_Create_1()
+        {
+            Assert.Equals(Interprete("Create"), 1);
+
+        }
+        [TestMethod]
+        public void SessionClassTest_NameProperty()
+        {
+
+
+            var session = new Session();
+
+            session.name = "test";
+
+            Assert.Equals(session.name, "test");
+
+
+
+        }
+        
+        
+
+        
     }
 }
